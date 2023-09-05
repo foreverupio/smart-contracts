29892ef0a8e9c667b8d9311a5f85719c2aa0f447
gabrielstoica <marius_gabriel1998@yahoo.com>
2023-09-05 14:06:26 +0300

    build: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { INFURA_API_KEY, FORKING_BLOCK_NUMMBER, getNetworkConfig } from "./config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // https://hardhat.org/hardhat-network/docs/guides/forking-other-networks
+      forking: {
+        url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+        blockNumber: FORKING_BLOCK_NUMMBER,
+        enabled: false,
+      },
+    },
+    mainnet: getNetworkConfig("mainnet"),
+    polygon: getNetworkConfig("polygon"),
+    sepolia: getNetworkConfig("sepolia"),
+    mumbai: getNetworkConfig("mumbai"),
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100000,
+      },
+    },
+  },
 };
 
 export default config;
