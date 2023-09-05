71ab9bd2fc0b2b01e3b2234a35dd794c77e59d7b
gabrielstoica <marius_gabriel1998@yahoo.com>
2023-09-05 14:18:35 +0300

    style: add prettier config

--- config/index.ts
+++ config/index.ts
@@ -1,23 +1,23 @@
-import { NetworkUserConfig } from "hardhat/types";
-import { config as dotenvConfig } from "dotenv";
-import { resolve } from "path";
+import { NetworkUserConfig } from 'hardhat/types';
+import { config as dotenvConfig } from 'dotenv';
+import { resolve } from 'path';
 
-const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "../.env";
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || '../.env';
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
-export const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
+export const PRIVATE_KEY: string = process.env.PRIVATE_KEY || '';
 if (!PRIVATE_KEY) {
-  throw new Error("Private key is missing from the .env file. Aborting...");
+  throw new Error('Private key is missing from the .env file. Aborting...');
 }
 
 export const INFURA_API_KEY: string | undefined = process.env.INFURA_API_KEY;
 if (!INFURA_API_KEY) {
-  throw new Error("Infura API Key is missing from the .env file. Aborting...");
+  throw new Error('Infura API Key is missing from the .env file. Aborting...');
 }
 
 export const FORKING_BLOCK_NUMMBER: number = 42369566;
-export const POLYGONSCAN_API_KEY: string = process.env.POLYGONSCAN_API_KEY || "";
-export const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || "";
+export const POLYGONSCAN_API_KEY: string = process.env.POLYGONSCAN_API_KEY || '';
+export const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || '';
 
 interface networkConfig {
   live: boolean;
